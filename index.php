22c2fb76e181dc62a61d6798a5964761715a90b1
PalComTech <dedeharyanto817@gmail.com>
2022-10-05 21:16:34 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$angka1=20;
+$angka2=10;
+$tambah =$angka1+$angka2;
+$kurang =$angka1-$angka2;
+$kali =$angka1*$angka2;
+$bagi =$angka1/$angka2;
+echo 
+"angka1=".$angka1."<br>".
+"angka2=".$angka2."<br>".
+"Hasil tambah = ".$tambah."<br>".
+"Hasil kurang = ".$kurang."<br>".
+"Hasil kali = ".$kali."<br>".
+"Hasil bagi = ".$bagi."<br>";
+?>
